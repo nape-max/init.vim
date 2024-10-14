fa574539a95711b5cfa8586514b120dbcfe54789
Mak8s⚡️ <mak8s.cloud@gmail.com>
2024-10-14 20:46:11 +0300

    Enable more options for gopls

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -2,124 +2,155 @@ local status, nvim_lsp = pcall(require, "lspconfig")
 local configs = require("lspconfig.configs")
 local util = require("lspconfig.util")
 if not status then
-	return
+    return
 end
 
 require("vim.lsp.protocol")
 
 -- Inject Brief LSP
 if not configs.briefls then
-	configs.briefls = {
-		default_config = {
-			cmd = { "briefls" },
-			filetypes = { "brief" },
-			root_dir = function(fname)
-				return util.root_pattern(".git")(fname)
-			end,
-			single_file_support = true,
-			capabilities = {
-				workspace = {
-					didChangeWatchedFiles = {
-						dynamicRegistration = true,
-					},
-				},
-			},
-		},
-		settings = {},
-	}
+    configs.briefls = {
+        default_config = {
+            cmd = { "briefls" },
+            filetypes = { "brief" },
+            root_dir = function(fname)
+                return util.root_pattern(".git")(fname)
+            end,
+            single_file_support = true,
+            capabilities = {
+                workspace = {
+                    didChangeWatchedFiles = {
+                        dynamicRegistration = true,
+                    },
+                },
+            },
+        },
+        settings = {},
+    }
 end
 
 local initialize_lsp_which_keys
 local on_attach = function(client, bufnr)
-	local opts = { buffer = bufnr, remap = false }
-	-- format on save
-	if client.server_capabilities.documentFormattingProvider then
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("Format", { clear = true }),
-			buffer = bufnr,
-			callback = function()
-				vim.lsp.buf.format()
-			end,
-		})
-	end
+    local opts = { buffer = bufnr, remap = false }
+    -- format on save
+    if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("Format", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format()
+            end,
+        })
+    end
 
-	initialize_lsp_which_keys()
+    initialize_lsp_which_keys()
 
-	vim.keymap.set("n", "gd", function()
-		vim.lsp.buf.definition()
-	end, opts)
-	vim.keymap.set("n", "K", function()
-		vim.lsp.buf.hover()
-	end, opts)
-	vim.keymap.set("n", "[d", function()
-		vim.diagnostic.goto_next()
-	end, opts)
-	vim.keymap.set("n", "]d", function()
-		vim.diagnostic.goto_prev()
-	end, opts)
-	vim.keymap.set("n", "<C-h>", function()
-		vim.lsp.buf.signature_help()
-	end, opts)
+    vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition()
+    end, opts)
+    vim.keymap.set("n", "K", function()
+        vim.lsp.buf.hover()
+    end, opts)
+    vim.keymap.set("n", "[d", function()
+        vim.diagnostic.goto_next()
+    end, opts)
+    vim.keymap.set("n", "]d", function()
+        vim.diagnostic.goto_prev()
+    end, opts)
+    vim.keymap.set("n", "<C-h>", function()
+        vim.lsp.buf.signature_help()
+    end, opts)
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Lua
 nvim_lsp.lua_ls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-		},
-	},
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
 })
 
 -- TypeScript
 nvim_lsp.tsserver.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 -- JSON server
 nvim_lsp.jsonls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		filetypes = { "json", "jsonc" },
-		init_options = { provideFormatter = true },
-	},
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        filetypes = { "json", "jsonc" },
+        init_options = { provideFormatter = true },
+    },
 })
 
 -- CSS
 nvim_lsp.cssls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 -- Golang
 nvim_lsp.gopls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		gopls = {
-			gofumpt = true,
-			-- Required for correct LSP work inside Integrations tests.
-			buildFlags = { "-tags=integration" },
-		},
-	},
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+        gopls = {
+            gofumpt = true,
+            codelenses = {
+                gc_details = true,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+            },
+            hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+            },
+            analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+            },
+            usePlaceholders = true,
+            completeUnimported = true,
+            staticcheck = true,
+            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+            semanticTokens = true,
+            -- Required for correct LSP work inside Integrations tests.
+            buildFlags = { "-tags=integration" },
+        },
+    },
 })
 
 vim.filetype.add({ extension = { brief = "brief" } })
 -- Setup server
 nvim_lsp.briefls.setup({
-	on_attach = on_attach,
-	flags = {
-		debounce_text_changes = 150,
-	},
-	capabilities = capabilities,
+    on_attach = on_attach,
+    flags = {
+        debounce_text_changes = 150,
+    },
+    capabilities = capabilities,
 })
 
 -- nvim_lsp.sqlls.setup({
@@ -128,47 +159,47 @@ nvim_lsp.briefls.setup({
 -- })
 
 function initialize_lsp_which_keys()
-	local wk = require("which-key")
+    local wk = require("which-key")
 
-	wk.add({
-		{ "<leader>v", group = "Code" },
-		{ "<leader>vw", group = "Workspace" },
-		{
-			"<leader>vws",
-			function()
-				vim.lsp.buf.workspace_symbol()
-			end,
-			desc = "Find Symbol",
-		},
-		{
-			"<leader>vd",
-			function()
-				vim.diagnostic.open_float()
-			end,
-			desc = "Open Float",
-		},
-		{ "<leader>vc", group = "Code" },
-		{
-			"<leader>vca",
-			function()
-				vim.lsp.buf.code_action()
-			end,
-			desc = "Actions",
-		},
-		{ "<leader>vr", group = "Element" },
-		{
-			"<leader>vrr",
-			function()
-				vim.lsp.buf.references()
-			end,
-			desc = "References",
-		},
-		{
-			"<leader>vrn",
-			function()
-				vim.lsp.buf.rename()
-			end,
-			desc = "Rename",
-		},
-	})
+    wk.add({
+        { "<leader>v",  group = "Code" },
+        { "<leader>vw", group = "Workspace" },
+        {
+            "<leader>vws",
+            function()
+                vim.lsp.buf.workspace_symbol()
+            end,
+            desc = "Find Symbol",
+        },
+        {
+            "<leader>vd",
+            function()
+                vim.diagnostic.open_float()
+            end,
+            desc = "Open Float",
+        },
+        { "<leader>vc", group = "Code" },
+        {
+            "<leader>vca",
+            function()
+                vim.lsp.buf.code_action()
+            end,
+            desc = "Actions",
+        },
+        { "<leader>vr", group = "Element" },
+        {
+            "<leader>vrr",
+            function()
+                vim.lsp.buf.references()
+            end,
+            desc = "References",
+        },
+        {
+            "<leader>vrn",
+            function()
+                vim.lsp.buf.rename()
+            end,
+            desc = "Rename",
+        },
+    })
 end
--- lua/maps.lua
+++ lua/maps.lua
@@ -1,2 +1,9 @@
 vim.keymap.set("n", "<C-w>", "<cmd>:wincmd w<cr>")
 vim.keymap.set("n", "<C-p>", "<cmd>:wincmd p<cr>")
+
+-- Перенос параметров на отдельные строки в Golang
+vim.keymap.set(
+    "n",
+    "<leader>s",
+    "_/[a-zA-Z](<CR>vi(<Esc>%v$:s/\\%V(\\(.*\\))\\( [^{]\\)\\?/(\\r\\1,\\r)\\2<CR>v%:s/\\%V, /,\\r<CR>vi):s/\\%V\\([a-z]\\)/\\t\\1<CR>"
+)
