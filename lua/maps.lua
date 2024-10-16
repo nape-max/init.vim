0ebe5d414dadd93ab28bf1ed516544d8d677f15d
Mak8s⚡️ <mak8s.cloud@gmail.com>
2024-10-16 15:15:03 +0300

    Simplify inline to multiline through code actions

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,12 +1,9 @@
 vim.keymap.set("n", "<C-w>", "<cmd>:wincmd w<cr>")
 vim.keymap.set("n", "<C-p>", "<cmd>:wincmd p<cr>")
 
--- Перевод inline аргументов в многострочные в Golang
-vim.keymap.set(
-	"n",
-	"<leader>s",
-	"_/[a-zA-Z](<CR>vi(<Esc>%v$:s/\\%V(\\(.*\\))\\( [^{]\\)\\?/(\\r\\1,\\r)\\2<CR>v%:s/\\%V, /,\\r/g<CR>vi):s/\\%V\\([a-z]\\)/\\t\\1<CR>"
-)
+-- Упрощение перевода inline аргументов в многострочные в Golang,
+-- но происходит только вызов Code Actions в нужном месте
+vim.keymap.set("n", "<leader>s", "_/[a-zA-Z](<CR>vi(<Esc>%<Right>:lua vim.lsp.buf.code_action()<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
