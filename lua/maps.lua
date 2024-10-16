9521e970222a7954cfae1543147abe0e1ad72c7e
Mak8s⚡️ <mak8s.cloud@gmail.com>
2024-10-16 13:27:21 +0300

    Resolve conflicts

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,9 +1,12 @@
 vim.keymap.set("n", "<C-w>", "<cmd>:wincmd w<cr>")
 vim.keymap.set("n", "<C-p>", "<cmd>:wincmd p<cr>")
 
--- Перенос параметров на отдельные строки в Golang
+-- Перевод inline аргументов в многострочные в Golang
 vim.keymap.set(
-    "n",
-    "<leader>s",
-    "_/[a-zA-Z](<CR>vi(<Esc>%v$:s/\\%V(\\(.*\\))\\( [^{]\\)\\?/(\\r\\1,\\r)\\2<CR>v%:s/\\%V, /,\\r<CR>vi):s/\\%V\\([a-z]\\)/\\t\\1<CR>"
+	"n",
+	"<leader>s",
+	"_/[a-zA-Z](<CR>vi(<Esc>%v$:s/\\%V(\\(.*\\))\\( [^{]\\)\\?/(\\r\\1,\\r)\\2<CR>v%:s/\\%V, /,\\r/g<CR>vi):s/\\%V\\([a-z]\\)/\\t\\1<CR>"
 )
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
