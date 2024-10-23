0571357befc401a3a683db78f6f3faeffa71b6c9
Mak8s⚡️ <mak8s.cloud@gmail.com>
2024-10-23 21:11:10 +0300

    Added transparent plugin

--- lua/settings.lua
+++ lua/settings.lua
@@ -43,3 +43,9 @@ local opt = vim.opt
 
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- Background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
